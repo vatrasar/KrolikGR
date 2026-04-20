463158a51816c4abe35ea4f7d2e9bb765ce75632
szymon <vatrasar@gmail.com>
2026-04-20 23:41:49 +0200

    add button to change theme from dark to light

--- Src/Features/Shell/UI/Screens/Host/HostViewModel.cs
+++ Src/Features/Shell/UI/Screens/Host/HostViewModel.cs
@@ -1,8 +1,11 @@
-using System;
-using System.Reactive;
-using KrolikGR.Src.Core.Mvvm;
 using ReactiveUI;
 using ReactiveUI.SourceGenerators;
+using Avalonia;
+using Avalonia.Styling;
+using Material.Icons;
+using System.Reactive.Linq;
+using KrolikGR.Src.Core.Mvvm;
+
 
 namespace KrolikGR.Src.Features.Shell.UI.Screens.Host;
 
@@ -11,6 +14,8 @@ public partial class HostViewModel : ViewModelBase, IScreen
     [Reactive]
     private bool _isMenuCollapsed;
 
+    public MaterialIconKind ThemeIcon => IsDarkTheme ? MaterialIconKind.WeatherSunny : MaterialIconKind.WeatherNight;
+
     public RoutingState Router { get; }
 
     public HostViewModel(RoutingState router)
@@ -18,9 +23,26 @@ public partial class HostViewModel : ViewModelBase, IScreen
         Router = router;
     }
 
+
+    private bool IsDarkTheme => Application.Current?.ActualThemeVariant == ThemeVariant.Dark;
+
     [ReactiveCommand]
     private void ToggleMenu()
     {
         IsMenuCollapsed = !IsMenuCollapsed;
     }
+
+    [ReactiveCommand]
+    private void ToggleTheme()
+    {
+        if (Application.Current is not null)
+        {
+            Application.Current.RequestedThemeVariant = IsDarkTheme 
+                ? ThemeVariant.Light 
+                : ThemeVariant.Dark;
+            
+            this.RaisePropertyChanged(nameof(ThemeIcon));
+        }
+    }
 }
+
